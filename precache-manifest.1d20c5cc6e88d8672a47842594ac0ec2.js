418f6420d3bd1fc81ab4ee8a1e19b77be2231075
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-06-22 13:34:59 +0530

    Updates

---
+++ precache-manifest.1d20c5cc6e88d8672a47842594ac0ec2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9488cf5174b8301abb491529581c9ff5",
+    "url": "/MyReads-Udacity-ReactJS/index.html"
+  },
+  {
+    "revision": "3378f49dca055c270f6c",
+    "url": "/MyReads-Udacity-ReactJS/static/css/main.e8763083.chunk.css"
+  },
+  {
+    "revision": "d46f0c8b93d375071f59",
+    "url": "/MyReads-Udacity-ReactJS/static/js/2.49adc8ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyReads-Udacity-ReactJS/static/js/2.49adc8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3378f49dca055c270f6c",
+    "url": "/MyReads-Udacity-ReactJS/static/js/main.72147c06.chunk.js"
+  },
+  {
+    "revision": "761850c0e837833959c9",
+    "url": "/MyReads-Udacity-ReactJS/static/js/runtime-main.3dfd499c.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads-Udacity-ReactJS/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads-Udacity-ReactJS/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads-Udacity-ReactJS/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
